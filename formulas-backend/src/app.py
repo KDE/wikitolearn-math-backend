07f1e1081800afe312252b4f67d2d1e63c73fe82
Alessandro Tundo <alessandrotundo94@gmail.com>
2018-06-12 01:30:05 +0200

    [formulas-backend] Added Swagger doc
    Signed-off-by: Alessandro Tundo <alessandrotundo94@gmail.com>

--- formulas-backend/src/app.py
+++ formulas-backend/src/app.py
@@ -4,6 +4,7 @@ import os,sys
 from eve import Eve
 from flask import jsonify
 import json
+from eve_swagger import swagger, add_documentation
 
 mongo_host = os.environ.get('MONGO_HOST')
 service_port = 8087
@@ -13,10 +14,28 @@ eve_settings['MONGO_HOST'] = mongo_host
 eve_settings['MONGO_DBNAME'] = 'formulas'
 eve_settings['DEBUG'] = True
 
+eve_settings['SWAGGER_INFO'] = {
+    'title': 'TexLive backend service',
+    'version': '1.0',
+    'description': '',
+    'termsOfService': '',
+    'contact': {
+        'name': 'WikiTolearn.org',
+        'url': 'mailto:sysadmins@wikitolearn.org'
+    },
+    'license': {
+        'name': 'AGPLv3+',
+        'url': 'https://www.gnu.org/licenses/agpl-3.0.html',
+    },
+    'schemes': ['http', 'https'],
+}
+
 eve_settings['SOFT_DELETE'] = True
 eve_settings['API_VERSION'] = 'v1'
 eve_settings['VERSIONING'] = False
 eve_settings['RENDERERS'] = ['eve.render.JSONRenderer']
+# It raise a warning but it needs to generate a valid Swagger doc
+eve_settings['XML'] = False
 eve_settings['RESOURCE_METHODS'] = ['GET', 'POST']
 eve_settings['ITEM_METHODS'] = ['GET', 'PATCH', 'DELETE']
 eve_settings['EXTENDED_MEDIA_INFO'] = ['content_type', 'name', 'length']
@@ -39,6 +58,28 @@ eve_settings['DOMAIN']['formulas'] = {
 
 
 app = Eve(settings=eve_settings)
+app.register_blueprint(swagger)
+
+# Update Swagger doc
+add_documentation({
+  'paths': {
+    '/rendered-formulas/{renderedFormulaId}': {
+      'get': {
+        'parameters': [{
+          'in': 'path',
+          'name': 'renderedFormulaId',
+          'required': True,
+          'description': 'The id of the svg media',
+          'type': 'string',
+        }],
+        'responses': {
+          '200': {
+            'description': 'Formula svg fetched successfully',
+            'type': 'media',
+
+          }
+        }
+}}}})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=service_port)
